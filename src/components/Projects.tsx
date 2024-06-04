1afa76846b40df3708f1312ae0e762ff4235437c
ushima09 <hanaprgrm1997@gmail.com>
2024-06-04 16:56:04 +0900

    Projectsコンポーネント調整

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,14 +1,17 @@
-import { Container, Paper, Typography } from '@mui/material';
+import { Box, Container, Paper, Typography } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
+import Image from 'next/image';
 
 const projectItems = [
   {
     title: 'ポートフォリオ',
-    desc: 'このサイトです。',
+    image: '/image/project_01.png',
+    desc: 'このサイトです。Next.jsとMaterial UIで実装しています。コード品質担保のためESLint・Prettierを導入し、保存時に自動フォーマットされるよう設定しています。',
   },
   {
     title: 'gulp環境',
-    desc: 'HTML/CSS/JSのページ作成用の環境です。',
+    image: '',
+    desc: 'HTML/CSS（Scss）/JSのページ作成用の環境です。localhostで表示確認ができるようBrowser-Syncを導入しています。コード品質担保のため各種リンターを導入しています。',
   },
 ];
 const Projects = () => {
@@ -21,8 +24,9 @@ const Projects = () => {
       <Typography
         variant="h2"
         align="center"
+        sx={{ mb: 3 }}
       >
-        Projects
+        プロジェクト
       </Typography>
       <Grid
         container
@@ -37,10 +41,36 @@ const Projects = () => {
             <Paper
               elevation={3}
               sx={{
-                p: 2,
+                p: {
+                  xs: 2,
+                  sm: 3,
+                },
               }}
             >
-              <Typography>{item.title}</Typography>
+              <Typography
+                variant="h3"
+                sx={{
+                  mb: 2,
+                }}
+              >
+                {item.title}
+              </Typography>
+              <Box
+                sx={{
+                  position: 'relative',
+                  display: 'block',
+                  borderRadius: 1,
+                  overflow: 'hidden',
+                }}
+              >
+                <Image
+                  src={item.image}
+                  layout="intrinsic"
+                  width={2880}
+                  height={1576}
+                  alt=""
+                />
+              </Box>
               <Typography>{item.desc}</Typography>
             </Paper>
           </Grid>
